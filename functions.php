668463955aa16490c855a04dde3b736f1710e867
Sai kumar <sai@softscripts.net>
2017-10-02 15:28:04 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
-include('inc/builder.php'); // Enable The Builder for All Custom Post Types
+include('builder.php'); // Enable The Builder for All Custom Post Types
 
 /* Comment the following line if you want to enable only the builder */
 
-include('inc/box.php'); // Enable Divi Settings for All Custom Post Type
+include('box.php'); // Enable Divi Settings for All Custom Post Type
 
 /* Add your custom functions after this line*/
